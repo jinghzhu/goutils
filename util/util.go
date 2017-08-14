e1218ccf546be7280eb76a884782c96ba8f85d1b
jinghuaz <jhzhu@outlook.com>
2017-08-14 17:17:27 +0800

    Update Struct2Json to Struct2String so that it can always return a data

--- util/util.go
+++ util/util.go
@@ -2,16 +2,36 @@ package util
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
+	"os/exec"
+	"strings"
 )
 
-func Struct2Json(v interface{}) ([]byte, error) {
+func Struct2String(v interface{}) string {
 	result, err := json.Marshal(v)
 	if err != nil {
 		errMsg := "Fail to translate to json"
 		fmt.Println(errMsg)
-		return []byte(``), errors.New(errMsg)
+		return fmt.Sprintf("%v", v)
 	}
-	return result, nil
+	return string(result)
+}
+
+// GetMountPoints returns all moutpoins in a string array
+func GetMountPoints(server string) ([]string, error) {
+	b, err := exec.Command("showmount", "-e", server).Output()
+	if err != nil {
+		fmt.Println("error in showmount: " + err.Error())
+		return nil, err
+	}
+	s := strings.TrimSpace(string(b))
+	// The fist line of showmount -e <server> is Exports list on <server>
+	firstLine := strings.Index(s, "\n")
+	sArr := strings.Split(s[firstLine+1:], "\n")
+	for i := 0; i < len(sArr); i++ {
+		index := strings.Index(sArr[i], " ")
+		temp := sArr[i]
+		sArr[i] = temp[:index]
+	}
+	return sArr, nil
 }
