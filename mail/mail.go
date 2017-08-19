65f24e3990243988202987ab60d84a33f40fdf58
jinghuaz <jhzhu@outlook.com>
2017-08-19 15:14:47 +0800

    Make the mail send method accept sender name

--- mail/mail.go
+++ mail/mail.go
@@ -2,6 +2,7 @@ package mail
 
 import (
 	"errors"
+	"fmt"
 	"net/smtp"
 	"strings"
 
@@ -13,13 +14,18 @@ const (
 	contentTypeText = "Content-Type: text/plain; charset=UTF-8"
 )
 
-func Send(from string, to []string, subject string, body string) (err error) {
+func Send(from string, fromAddr string, to []string, subject string, body string) (err error) {
 	defer catcher.CatchPanic(&err)
 	if len(to) == 0 {
 		return errors.New("Need 1 recipient at least.")
 	}
-	msg := []byte("To: " + strings.Join(to, ",") + "\r\nFrom: " +
-		"<" + from + ">\r\nSubject: " + subject + "\r\n" + contentTypeText + "\r\n\r\n" + body)
+	if from == "" || !strings.Contains(fromAddr, "@") {
+		errMsg := fmt.Sprintf("The mail sender is not correct: from = %s, from_address = %q\n", from, fromAddr)
+		return errors.New(errMsg)
+	}
+
+	msg := []byte("To: " + strings.Join(to, ",") + "\r\nFrom: " + from +
+		"<" + fromAddr + ">\r\nSubject: " + subject + "\r\n" + contentTypeText + "\r\n\r\n" + body)
 	return smtp.SendMail(server, nil, from, to, msg)
 
 }
