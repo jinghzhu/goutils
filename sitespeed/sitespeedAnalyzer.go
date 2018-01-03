1ddd0cdbee567ee4a21e22d3861eddcd70d11fd4
jinghuaz <jhzhu@outlook.com>
2018-01-03 17:14:48 +0800

    Format package sitespeed

--- sitespeed/sitespeedAnalyzer.go
+++ sitespeed/sitespeedAnalyzer.go
@@ -9,81 +9,11 @@ import (
 	"strconv"
 )
 
-const CountRule = 39   // num of sitespeed rules
-const Threshold50 = 50 // used for analyze the percent of data above scroe 50
-const WorstScore = 0   // the bottom score of performance budget
-const BestScore = 90   // the top score of performance budget
-const RulesNum = 39
-
-// the following 3 strcuts are used to analyze performance budget based on sitespeed.io
-// for more details, please read example test.json
-type PerformanceBudget struct {
-	CookieUrl string          `json:"cookieUrl,omitempty"`
-	BasUrl    string          `json:"baseUrl"`
-	Timeout   int             `json:"mochaTimeout,omitempty"`
-	Id        string          `json:"id,omitempty"`
-	Pw        string          `json:"pw,omitempty"`
-	TestCases []JsonTestCases `json:"testCases"`
-}
-
-type JsonTestCases struct {
-	Description string                 `json:"description,omitempty"`
-	Pathname    string                 `json:"pathname"`
-	UrlParams   map[string]interface{} `json:"urlParams"`
-	Budget      JsonBudget             `json:"budget"`
-}
-
-type JsonBudget struct {
-	Rules JsonRules `json:"rules"`
-}
-
-// all sitespeed.io rules
-type JsonRules struct {
-	Criticalpath                int `json:"criticalpath"`
-	Spof                        int `json:"spof"`
-	Cssnumreq                   int `json:"cssnumreq"`
-	Cssimagesnumreq             int `json:"cssimagesnumreq"`
-	Jsnumreq                    int `json:"jsnumreq"`
-	Yemptysrc                   int `json:"yemptysrc"`
-	Ycompress                   int `json:"ycompress"`
-	Ycsstop                     int `json:"ycsstop"`
-	Yjsbottom                   int `json:"yjsbottom"`
-	Yexpressions                int `json:"yexpressions"`
-	Ydns                        int `json:"ydns"`
-	Yminify                     int `json:"yminify"`
-	Redirects                   int `json:"redirects"`
-	Noduplicates                int `json:"noduplicates"`
-	Yetags                      int `json:"yetags"`
-	Yxhr                        int `json:"yxhr"`
-	Yxhrmethod                  int `json:"yxhrmethod"`
-	Mindom                      int `json:"mindom"`
-	Yno404                      int `json:"yno404"`
-	Ymincookie                  int `json:"ymincookie"`
-	Ycookiefree                 int `json:"ycookiefree"`
-	Ynofilter                   int `json:"ynofilter"`
-	Avoidscalingimages          int `json:"avoidscalingimages"`
-	Yfavicon                    int `json:"yfavicon"`
-	Thirdparyasyncjs            int `json:"thirdpartyasyncjs"`
-	Csspring                    int `json:"cssprint"`
-	Cssinheaddomain             int `json:"cssinheaddomain"`
-	Syncjsinhead                int `json:"syncjsinhead"`
-	Avoidfont                   int `json:"avoidfont"`
-	Totalrequests               int `json:"totalrequests"`
-	Expiresmod                  int `json:"expiresmod"`
-	Longexpirehead              int `json:"longexpirehead"`
-	Nodnslookupswhenfewrequests int `json:"nodnslookupswhenfewrequests"`
-	Inlinecsswhenfewrequest     int `json:"inlinecsswhenfewrequest"`
-	Textcontent                 int `json:"textcontent"`
-	Thirdpartyversions          int `json:"thirdpartyversions"`
-	Ycdn                        int `json:"ycdn"`
-	Connectionclose             int `json:"connectionclose"`
-	Privateheaders              int `json:"privateheaders"`
-}
-
-func main() {
+// Demo shows how to use it.
+func Demo() {
 	file, err := ioutil.ReadFile("./sitespeedAnalyzer.json")
 	if err != nil {
-		fmt.Println("File error: %v\n", err)
+		fmt.Printf("File error: %v\n", err)
 		return
 	}
 
@@ -91,23 +21,23 @@ func main() {
 	err = json.Unmarshal(file, &budgets)
 
 	if err != nil {
-		fmt.Println("Unmarshal error: %v\n", err)
+		fmt.Printf("Unmarshal error: %v\n", err)
 		return
 	}
 
 	err = GetBudgetStatics(budgets)
-	// err = GetRuleScore(budgets)
 	if err != nil {
-		fmt.Println("Analysis error: %v\n", err)
+		fmt.Printf("Analysis error: %v\n", err)
 		return
 	}
 
 }
 
+// GetRuleScore get pre-defined budget.
 // For a site, as it have many paths, sitespeed.io would generate many performance budgets.
 // This function is trying to calcute the average value in each rule field.
 func GetRuleScore(budgets PerformanceBudget) error {
-	var countBudget int = len(budgets.TestCases)
+	countBudget := len(budgets.TestCases)
 	if countBudget < 1 {
 		errMsg := "Error! 0 test cases"
 		fmt.Println(errMsg)
@@ -120,7 +50,7 @@ func GetRuleScore(budgets PerformanceBudget) error {
 		ruleField := ruleList.Type().Field(i)
 		ruleName := ruleField.Tag
 		fmt.Println("\nready to deal with rule: " + ruleName.Get("json"))
-		var sum int64 = 0
+		var sum int64
 		for j := 0; j < countBudget; j++ {
 			values := reflect.ValueOf(budgets.TestCases[j].Budget.Rules)
 			value := values.Field(i)
@@ -134,6 +64,7 @@ func GetRuleScore(budgets PerformanceBudget) error {
 	return nil
 }
 
+// GetBudgetStatics calculats the statics of performance.
 // As mentioned before, if there are more than 1 paths set in test.json,
 // sitespeed would generate more than one performance results (one for each path).
 // This is used to analyze performance test result. It provides:
@@ -143,7 +74,7 @@ func GetRuleScore(budgets PerformanceBudget) error {
 // (4) how many paths meet (3);
 // (5) the percent of rule scores in range (-1, 50) and [50, 90) respectively
 func GetBudgetStatics(budgets PerformanceBudget) error {
-	var countBudget int = len(budgets.TestCases)
+	countBudget := len(budgets.TestCases)
 	if countBudget < 1 {
 		errMsg := "Error! 0 test cases"
 		fmt.Println(errMsg)
@@ -152,7 +83,7 @@ func GetBudgetStatics(budgets PerformanceBudget) error {
 
 	var max, min, countVal049, countVal5089, count, mean, countBestSco, countWorstSco int64 = -1, 90, 0, 0, 0, 0, 0, 0
 
-	var total int64 = 0
+	var total int64
 
 	for i := 0; i < countBudget; i++ {
 		values := reflect.ValueOf(budgets.TestCases[i].Budget.Rules)
@@ -183,14 +114,14 @@ func GetBudgetStatics(budgets PerformanceBudget) error {
 					fmt.Println("******")
 					fmt.Println("BestScore: " + rule.Get("json") + strconv.FormatInt(val, 10))
 					fmt.Println("******")
-					fmt.Println("\n\n")
+					fmt.Println("")
 				} else {
 					countVal5089++
 
 					fmt.Println("******")
 					fmt.Println("[50, 90): " + rule.Get("json") + strconv.FormatInt(val, 10))
 					fmt.Println("******")
-					fmt.Println("\n\n")
+					fmt.Println("")
 				}
 			} else {
 				if val == WorstScore {
@@ -198,14 +129,14 @@ func GetBudgetStatics(budgets PerformanceBudget) error {
 					fmt.Println("******")
 					fmt.Println("WorstScore: " + rule.Get("json") + strconv.FormatInt(val, 10))
 					fmt.Println("******")
-					fmt.Println("\n\n")
+					fmt.Println("")
 					countWorstSco++
 				} else {
 
 					fmt.Println("******")
 					fmt.Println("(-1, 50): " + rule.Get("json") + strconv.FormatInt(val, 10))
 					fmt.Println("******")
-					fmt.Println("\n\n")
+					fmt.Println("")
 					countVal049++
 				}
 			}
@@ -223,29 +154,29 @@ func GetBudgetStatics(budgets PerformanceBudget) error {
 
 	fmt.Println("max = " + strconv.FormatInt(max, 10))
 	fmt.Println("min = " + strconv.FormatInt(min, 10))
-	fmt.Println("\n\n\n\n\n\n\n\n\n")
+	fmt.Println("")
 
 	fmt.Println("Num of rules data = " + strconv.FormatInt(count, 10))
-	fmt.Println("\n")
+	fmt.Println("")
 
 	fmt.Println("Mean score of performance = " + strconv.FormatInt(mean, 10))
-	fmt.Println("\n")
+	fmt.Println("")
 
 	fmt.Println("Num of worst performance rules(rule.score = 0 ) = " + strconv.FormatInt(countWorstSco, 10))
 	fmt.Println("Percent of worst performance rules = ", valWorstPct*100)
-	fmt.Println("\n")
+	fmt.Println("")
 
 	fmt.Println("Num of best performance rules( = 90 ) = " + strconv.FormatInt(countBestSco, 10))
 	fmt.Println("Percent of best performance rules = ", valBestPct*100)
-	fmt.Println("\n")
+	fmt.Println("")
 
 	fmt.Println("Num of rules whose score is between 50 and 90([50, 90]) = " + strconv.FormatInt(countVal5089, 10))
 	fmt.Println("Percent of rules whose score is between 50 and 90([50, 90]) = ", val5089Pct*100)
-	fmt.Println("\n")
+	fmt.Println("")
 
 	fmt.Println("Num of rules whose score is between 0 and 50([0, 50)) = " + strconv.FormatInt(countVal049, 10))
 	fmt.Println("Percent of rules whose score is between 0 and 50([0, 50)) = ", val049Pct*100)
-	fmt.Println("\n")
+	fmt.Println("")
 
 	return nil
 }
