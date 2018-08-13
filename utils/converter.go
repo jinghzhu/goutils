2f6e01d715abbd93e2c7167b42ac01c91f9e8751
Jinghua Zhu <jhzhu@outlook.com>
2018-08-13 20:23:40 +0800

    Add ToFloat

--- utils/converter.go
+++ utils/converter.go
@@ -3,16 +3,17 @@ package utils
 import (
     "encoding/json"
     "fmt"
+    "strconv"
 )
 
-// ToString convert the input to a string.
+// ToString converts input to string.
 func ToString(obj interface{}) string {
 	res := fmt.Sprintf("%v", obj)
   
 	return string(res)
 }
 
-// ToJSON convert the input to a valid JSON string
+// ToJSON converts input to JSON string.
 func ToJSON(obj interface{}) (string, error) {
 	res, err := json.Marshal(obj)
 	if err != nil {
@@ -20,4 +21,14 @@ func ToJSON(obj interface{}) (string, error) {
 	}
 	
 	return string(res), nil
+}
+
+// ToFloat converts string to float, or 0.0 if the input is not a float.
+func ToFloat(str string) (float64, error) {
+	res, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		res = 0.0
+	}
+	
+	return res, err
 }
