7f974a8860485dd7f6d57c5dc4748a4506113c1f
Jinghua Zhu <jhzhu@outlook.com>
2018-08-13 20:46:31 +0800

    Add TestToBool

--- utils/converter_test.go
+++ utils/converter_test.go
@@ -5,6 +5,18 @@ import (
 	"testing"
 )
 
+func TestToBool(t *testing.T) {
+	testCases := []string{"true", "1", "True", "false", "0", "abcdef"}
+	expected := []bool{true, true, true, false, false, false}
+	for k, v := range testCases {
+		res, _ := ToBool(v)
+		if res != expected[k] {
+			t.Log("Case ", k, ": expected ", expected[k], " when result is ", res)
+			t.FailNow()
+		}
+	}
+}
+
 func TestToFloat(t *testing.T) {
 	testCasess := []string{"", "1", "-.01342", "10.", "empty", "1.23e3", ".23e10"}
 	expected := []float64{0, 1, -0.01342, 10.0, 0, 1230, 0.23e10}
