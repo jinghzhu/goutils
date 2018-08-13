a0041490fd34e8665254fe2746346c894305d810
Jinghua Zhu <jhzhu@outlook.com>
2018-08-13 20:38:34 +0800

    Add TestToJSON

--- utils/converter_test.go
+++ utils/converter_test.go
@@ -16,3 +16,21 @@ func TestToFloat(t *testing.T) {
 		}
 	}
 }
+
+func TestToJSON(t *testing.T) {
+	testCasess := []interface{}{"test", map[string]string{"a": "b", "b": "c"}, func() error { return fmt.Errorf("Error") }}
+	expected := [][]string{
+		{"\"test\"", "<nil>"},
+		{"{\"a\":\"b\",\"b\":\"c\"}", "<nil>"},
+		{"", "json: unsupported type: func() error"},
+	}
+	for i, test := range tests {
+		actual, err := ToJSON(test)
+		if actual != expected[i][0] {
+			t.Errorf("Expected toJSON(%v) to return '%v', got '%v'", test, expected[i][0], actual)
+		}
+		if fmt.Sprintf("%v", err) != expected[i][1] {
+			t.Errorf("Expected error returned from toJSON(%v) to return '%v', got '%v'", test, expected[i][1], fmt.Sprintf("%v", err))
+		}
+	}
+}
