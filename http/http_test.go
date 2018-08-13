39ba43b34b2ba40525df3c4afca2b63c0cf95297
Jinghua Zhu <jhzhu@outlook.com>
2018-08-13 20:51:34 +0800

    Add TestIsRequestURI

--- http/http_test.go
+++ http/http_test.go
@@ -69,3 +69,51 @@ func TestIsRequestURL(t *testing.T) {
 		}
 	}
 }
+
+func TestIsRequestURI(t *testing.T) {
+	t.Parallel()
+
+	var tests = []struct {
+		param    string
+		expected bool
+	}{
+		{"", false},
+		{"http://foo.bar/#com", true},
+		{"http://foobar.com", true},
+		{"https://foobar.com", true},
+		{"foobar.com", false},
+		{"http://foobar.coffee/", true},
+		{"http://foobar.中文网/", true},
+		{"http://foobar.org/", true},
+		{"http://foobar.org:8080/", true},
+		{"ftp://foobar.ru/", true},
+		{"http://user:pass@www.foobar.com/", true},
+		{"http://127.0.0.1/", true},
+		{"http://duckduckgo.com/?q=%2F", true},
+		{"http://localhost:3000/", true},
+		{"http://foobar.com/?foo=bar#baz=qux", true},
+		{"http://foobar.com?foo=bar", true},
+		{"http://www.xn--froschgrn-x9a.net/", true},
+		{"xyz://foobar.com", true},
+		{"invalid.", false},
+		{".com", false},
+		{"rtmp://foobar.com", true},
+		{"http://www.foo_bar.com/", true},
+		{"http://localhost:3000/", true},
+		{"http://foobar.com/#baz=qux", true},
+		{"http://foobar.com/t$-_.+!*\\'(),", true},
+		{"http://www.foobar.com/~foobar", true},
+		{"http://www.-foobar.com/", true},
+		{"http://www.foo---bar.com/", true},
+		{"mailto:someone@example.com", true},
+		{"irc://irc.server.org/channel", true},
+		{"/abs/test/dir", true},
+		{"./rel/test/dir", false},
+	}
+	for _, test := range tests {
+		actual := IsRequestURI(test.param)
+		if actual != test.expected {
+			t.Errorf("Expected IsRequestURI(%q) to be %v, got %v", test.param, test.expected, actual)
+		}
+	}
+}
