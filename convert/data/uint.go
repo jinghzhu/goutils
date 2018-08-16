5b3eb915d8dd86f9e357242ce8e438c4fb6fefe3
Jinghua Zhu <jhzhu@outlook.com>
2018-08-16 16:21:31 +0800

    Add valiadator

---
+++ convert/data/uint.go
@@ -0,0 +1,39 @@
+package data
+
+// UintPointer returns a pointer to of the uint value passed in.
+func UintPointer(v uint) *uint {
+	return &v
+}
+
+// UintVal returns the value of the uint pointer passed in or
+// 0 if the pointer is nil.
+func UintVal(v *uint) uint {
+	if v != nil {
+		return *v
+	}
+
+	return 0
+}
+
+// UintSlice converts a slice of uint values uinto a slice of
+// uint pointers
+func UintSlice(src []uint) []*uint {
+	dst := make([]*uint, len(src))
+	for i := 0; i < len(src); i++ {
+		dst[i] = &(src[i])
+	}
+
+	return dst
+}
+
+// UintValSlice converts a slice of uint pointers uinto a slice of
+// uint values
+func UintValSlice(src []*uint) []uint {
+	dst := make([]uint, len(src))
+	for i := 0; i < len(src); i++ {
+		if src[i] != nil {
+			dst[i] = *(src[i])
+		}
+	}
+	return dst
+}
