8d37ce680c48652eb257c60a2602a837fe4cb146
Jinghua Zhu <jhzhu@outlook.com>
2018-08-16 16:34:43 +0800

    Add various int type to string

--- converter/string.go
+++ converter/string.go
@@ -2,7 +2,22 @@ package converter
 
 import "strconv"
 
-// StrToInt64 turns an int64 into a string
-func StrToInt64(value int64) string {
+// Int64ToStr turns an int64 into a string.
+func Int64ToStr(value int64) string {
 	return strconv.FormatInt(value, 10)
+}
+
+// Int32ToStr turns an int32 into a string.
+func Int32ToStr(value int32) string {
+	return strconv.Itoa(int(value))
+}
+
+// Int16ToStr turns an int16 into a string.
+func Int16ToStr(value int16) string {
+	return strconv.FormatInt(int64(value), 10)
+}
+
+// Int8ToStr turns an int8 into a string.
+func Int8ToStr(value int8) string {
+	return strconv.FormatInt(int64(value), 10)
 }
